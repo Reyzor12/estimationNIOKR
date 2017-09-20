2275b3853c3fe11c86d76c23872e805b804317e8
Reyzor <testaccwow001@gmail.com>
2017-09-20 20:03:21 +0300

    add re[prts BUT CHECK GIT EDITOR

---
+++ src/main/java/org/eleron/lris/niokr/controller/MainMenu.java
@@ -0,0 +1,23 @@
+package org.eleron.lris.niokr.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import org.eleron.lris.niokr.bussines.LoadScenes;
+
+public class MainMenu {
+
+    @FXML
+    private Button supportBtn;
+
+    @FXML
+    private Button addNewUser;
+
+    public void initialize(){
+
+    }
+
+    @FXML
+    public void addReport(){
+        LoadScenes.load("view/ReviewMenu.fxml");
+    }
+}
