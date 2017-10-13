cf02b4af0038ad4caab82fdda871e46281515dd6
Reyzor <testaccwow001@gmail.com>
2017-10-13 07:05:26 +0300

    watch findReport method and find out why don't work

--- src/main/java/org/eleron/lris/niokr/controller/MainMenu.java
+++ src/main/java/org/eleron/lris/niokr/controller/MainMenu.java
@@ -3,9 +3,7 @@ package org.eleron.lris.niokr.controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import org.eleron.lris.niokr.bussines.LoadScenes;
 import org.eleron.lris.niokr.bussines.ReportBussines;
@@ -40,9 +38,14 @@ public class MainMenu {
     private TableColumn<Report,String> columnName;
 
     @FXML
+    private TextField findReportField;
+
+    @FXML
     public void initialize(){
 
         refreshTable();
+
+
     }
 
     @FXML
@@ -62,16 +65,31 @@ public class MainMenu {
     }
 
     public void findReport(String request){
-        for(int i=0;i<tableView.getItems().size();i++){
+        tableView.setRowFactory(item->new TableRow<Report>(){
+            public void updateItem(Report report, boolean okey){
+                super.updateItem(report,okey);
+                if(request!="" && report.getNameShort().matches(request)){
+                    setStyle("-fx-background-color: tomato;");
+                } else{
+                    setStyle("");
+                }
+            }
+        });
+        /*for(int i=0;i<tableView.getItems().size();i++){
             if(tableView.getItems().get(i).getNameShort().matches(request)){
                 tableView.getColumns().
             }
-        }
+        }*/
     }
 
     public void refreshTable(){
         ObservableList<Report> reportsObs = FXCollections.observableArrayList(ReportBussines.loadReportMain());
         columnName.setCellValueFactory(new PropertyValueFactory<Report,String>("nameShort"));
         tableView.setItems(reportsObs);
+    }
+
+    @FXML
+    public void findReport(){
+        findReport(findReportField.getText());
     }
 }
