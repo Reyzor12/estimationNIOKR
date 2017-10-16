3b0677cfa6f8d195ae0ee3be6c4703025a860ea1
Reyzor12 <testaccwow001@gmail.com>
2017-10-16 16:43:44 +0300

    controller for fillReport start (not finish) + connect with fxml

---
+++ src/main/java/org/eleron/lris/niokr/controller/FillReportController.java
@@ -0,0 +1,44 @@
+package org.eleron.lris.niokr.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class FillReportController {
+
+    @FXML
+    private Button saveBtn;
+
+    @FXML
+    private Button cancelBtn;
+
+    @FXML
+    private Button sendBtn;
+
+    @FXML
+    private Label periodLabel;
+
+    @FXML
+    private Label departmentLabel;
+
+    @FXML
+    private Label ownerLabel;
+
+    @FXML
+    private Label nameReportLabel;
+
+    @FXML
+    private Label perYearLabel;
+
+    @FXML
+    private TextArea progressTArea;
+
+    @FXML
+    private TextArea troubleTArea;
+
+    @FXML
+    private void initialize(){
+
+    }
+}
