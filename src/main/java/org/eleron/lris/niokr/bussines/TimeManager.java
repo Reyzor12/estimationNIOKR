3a956f9b3ffb7b121c826a703e81590a78fb56c6
Reyzor <testaccwow001@gmail.com>
2017-10-28 01:00:12 +0300

    TimeManager Refactoring

--- src/main/java/org/eleron/lris/niokr/bussines/TimeManager.java
+++ src/main/java/org/eleron/lris/niokr/bussines/TimeManager.java
@@ -1,6 +1,7 @@
 package org.eleron.lris.niokr.bussines;
 
 import org.apache.log4j.Logger;
+import org.eleron.lris.niokr.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -9,44 +10,47 @@ import java.util.Date;
 
 public class TimeManager {
 
+    /*
+    * Logger
+    * */
+
     private static final Logger log = Logger.getLogger(TimeManager.class);
 
-    private static SessionFactory sessionFactory;
+    /*
+    * Static fields
+    * */
 
     private static Date date;
+
+    /*
+    * Getters Setters methods
+    * */
 
     public Date getDate() {
         return date;
     }
-
     public void setDate(Date date) {
         date = date;
     }
 
-    public SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
-
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        sessionFactory = sessionFactory;
-    }
-
+    /*
+    * Bussiness Functions
+    * */
 
     public static Date currentDate(){
 
         log.info("get date");
-        sessionFactory = Enter.getSessionFactory();
-
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
         try{
+            transaction = session.beginTransaction();
             date = (Date)session.createSQLQuery("SELECT GETDATE()").list().get(0);
             transaction.commit();
         } catch(Exception e){
             log.error("fail get date ", e);
             transaction.rollback();
         }finally{
-            session.close();
+            if(session.isOpen()) session.close();
         }
         return date;
     }
