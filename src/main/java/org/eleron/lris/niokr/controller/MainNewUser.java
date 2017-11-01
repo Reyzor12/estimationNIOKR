2a71b6f88bf131f527b2be189dc986ac7a2380bc
Reyzor <testaccwow001@gmail.com>
2017-11-01 11:29:56 +0300

    new User main not finish refactoring

--- src/main/java/org/eleron/lris/niokr/controller/MainNewUser.java
+++ src/main/java/org/eleron/lris/niokr/controller/MainNewUser.java
@@ -1,45 +1,37 @@
 package org.eleron.lris.niokr.controller;
 
-import com.microsoft.sqlserver.jdbc.SQLServerException;
-import com.sun.javafx.scene.control.behavior.ChoiceBoxBehavior;
-import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import org.apache.log4j.Logger;
 import org.eleron.lris.niokr.bussines.Enter;
 import org.eleron.lris.niokr.bussines.LoadScenes;
 import org.eleron.lris.niokr.bussines.UserBussines;
+import org.eleron.lris.niokr.dao.DepartmentDAO;
 import org.eleron.lris.niokr.dao.DepartmentDAOImplements;
-import org.eleron.lris.niokr.dao.UserDAOImplements;
 import org.eleron.lris.niokr.model.Department;
 import org.eleron.lris.niokr.model.User;
 import org.eleron.lris.niokr.util.HibernateUtil;
 import org.eleron.lris.niokr.util.ValidationUtil;
 import org.hibernate.SessionFactory;
 
-import javax.validation.ConstraintViolationException;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
 import java.util.List;
-import java.util.regex.Pattern;
 
 public class MainNewUser {
 
+    /*
+    * Logging
+    * */
+
     final static Logger log = Logger.getLogger(MainNewUser.class);
 
-    private Label nameLabel;
-    private Label snameLabel;
-    private Label fnameLabel;
-    private Label departmentLabel;
-
-    private User user;
-
-    private List<Department> departments;
-
-    private SessionFactory sessionFactory;
+    /*
+    * FXML Fields
+    * */
 
     @FXML
     private Label infoLabel;
@@ -55,31 +47,31 @@ public class MainNewUser {
     @FXML
     private Button saveButton;
 
+    /*
+    * Init method
+    * */
 
     @FXML
     private void initialize() {
         try{
             log.info("start initialize " + this.getClass().getName());
-            sessionFactory = HibernateUtil.getSessionFactory();
-            DepartmentDAOImplements departmentDAO = new DepartmentDAOImplements();
-            departments = departmentDAO.listDepartment();
-            ObservableList<Department>  departmentChoose = FXCollections.observableArrayList();
-            for (Department department: departments) {
-                departmentChoose.add(department);
-            }
-            departmentField.setItems(departmentChoose);
+            departmentField.setItems(
+                    FXCollections.observableArrayList(
+                            (new DepartmentDAOImplements())
+                                    .listDepartment()
+                    )
+            );
         } catch (Exception e) {
             log.error("initialize " + this.getClass().getName() + " fail",e);
-            e.printStackTrace();
         }
-
         ValidationUtil.validUserField(nameField);
         ValidationUtil.validUserField(snameField);
         ValidationUtil.validUserField(fnameField);
-
     }
 
-    public MainNewUser(){}
+    /*
+    * FXML Methods
+    * */
 
     @FXML
     public void addUser(){
