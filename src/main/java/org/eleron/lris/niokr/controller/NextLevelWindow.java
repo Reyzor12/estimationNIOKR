4d19eae334e43635cb4002ce29905786879fbe58
Reyzor12 <testaccwow001@gmail.com>
2017-11-07 16:58:40 +0300

    controller discr nextlevel

--- src/main/java/org/eleron/lris/niokr/controller/NextLevelWindow.java
+++ src/main/java/org/eleron/lris/niokr/controller/NextLevelWindow.java
@@ -1,10 +1,21 @@
 package org.eleron.lris.niokr.controller;
 
 import javafx.fxml.FXML;
+import org.apache.log4j.Logger;
 
 public class NextLevelWindow {
 
+    /*
+    * Logger
+    * */
+
+    private static final Logger log = Logger.getLogger(NextLevelWindow.class);
+
+    /*
+    * Init method
+    * */
+
     @FXML
     private void initialize(){}
-    
+
 }
